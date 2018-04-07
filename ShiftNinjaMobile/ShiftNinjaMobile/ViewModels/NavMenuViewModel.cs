1ab5a82bef89c557753a4aad2b0271492fd73568
Jerome Hurley <vonswirl@googlemail.com>
2018-04-07 16:56:27 +0100

    Icons, MenuItems, NavmenuViewModel added

---
+++ ShiftNinjaMobile/ShiftNinjaMobile/ViewModels/NavMenuViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ShiftNinjaMobile.ViewModels
+{
+    class NavMenuViewModel : DefaultViewModel
+
+      
+      
+    {
+        public ObservableCollection<NavMenuItems> MenuItems { get; set; }
+
+        public NavMenuViewModel() {
+            CanLoadMore = true;
+            Title = "Shift Ninja";
+
+            MenuItems = new ObservableCollection<NavMenuItems>();
+
+            MenuItems.Add(new NavMenuItems
+            {
+                Id = 0,
+                Title = "About Shift Ninja",
+                SelectMenuItem = MenuItem.AboutShiftNinja,
+                Icon = "icon.png"
+            });
+
+            MenuItems.Add(new NavMenuItems
+            {
+                Id = 1,
+                Title = "Calender",
+                SelectMenuItem = MenuItem.WorkCalender,
+                Icon = "workCalender.png"
+            });
+
+            MenuItems.Add(new NavMenuItems
+            {
+                Id = 2,
+                Title = "Shift History",
+                SelectMenuItem = MenuItem.ShiftHistory,
+                Icon = "history.png"
+            });  
+
+            MenuItems.Add(new NavMenuItems
+            {
+                Id = 3,
+                Title = "My Profile",
+                SelectMenuItem = MenuItem.UserProfile,
+                Icon = "userProfile.png"
+            });
+
+            MenuItems.Add(new NavMenuItems
+            {
+                Id = 4,
+                Title = "About Developer",
+                SelectMenuItem = MenuItem.AboutDeveloper,
+                Icon = "jeromeProfileSmall.png"
+            });
+        }
+    }
+}
